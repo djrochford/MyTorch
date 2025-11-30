073a987c7804cd0434581140def6437d5805e045
Damien Rochford <damien@admithub.com>
2025-11-29 21:32:58 -0500

    fix tests

--- tests/test.py
+++ tests/test.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from my_torch.linear_layer import Linear
 
 def test_instantiation_no_bias():
@@ -11,6 +13,13 @@ def test_instantiation_bias():
     assert my_layer.weights.shape == (4, 3)
     assert my_layer.bias.shape == (4,)
 
-def test_trivial_initialisation():
-    pass
-    
+def test_reset_params_resets():
+    my_layer = Linear(3, 4, True)
+    old_weights = np.copy(my_layer.weights)
+    old_bias = np.copy(my_layer.bias)
+    assert np.array_equal(old_weights, my_layer.weights)
+    assert np.array_equal(old_bias, my_layer.bias)
+    my_layer.reset_params(create_bias=True)
+    assert not np.array_equal(old_weights, my_layer.weights)
+    assert not np.array_equal(old_bias, my_layer.bias)
+
